5a373c2d5202ed5404fec17b90fd10940bee9323
samiur98 <sam@Shahs-MacBook-Pro.local>
2020-07-20 17:57:58 -0700

    client and server code seperated.

---
+++ client/src/dummyList.js
@@ -0,0 +1,40 @@
+const dummyList = [
+    {
+        id: 3,
+        title: 'cat'
+    },
+    {
+        id: 7,
+        title: 'dog'
+    },
+    {
+        id: 9,
+        title: 'parrot'
+    },
+    {
+        id: 19,
+        title: 'rabbit'
+    },
+    {
+        id: 23,
+        title: 'hamster'
+    },
+    {
+        id: 24,
+        title: 'cow'
+    },
+    {
+        id: 25,
+        title: 'tiger'
+    },
+    {
+        id: 27,
+        title: 'lion'
+    },
+    {
+        id: 30,
+        title: 'wolf'
+    }
+];
+
+export default dummyList;
